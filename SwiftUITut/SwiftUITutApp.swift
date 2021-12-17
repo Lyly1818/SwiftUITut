0080ac48aef0b3d35347a646a9c5265cdc4958af
Lesly Blaise <lpblaise@bhcc.edu>
2021-12-16 22:18:34 -0500

    Initial Commit

---
+++ SwiftUITut/SwiftUITutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutApp.swift
+//  SwiftUITut
+//
+//  Created by Lesly Blaise on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
